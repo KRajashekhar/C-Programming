84f259a7c29fe18db1b556f96925612c37d18f8d
Coursera Learner <nobody@nowhere.nul>
2020-07-27 15:27:38 +0000

    assignment 20

---
+++ 20_rot_matrix/rotate.c
@@ -0,0 +1,18 @@
+void rotate(char matrix[10][10]){
+  char copy[10][10];
+  for(int i = 0; i<10; i++)
+    {
+      for(int j=0;j<10;j++)
+	{
+	  copy[i][j] = matrix[i][j];
+	    }
+    }
+  for(int i=0; i<10; i++)
+    {
+      for(int j=0; j<10;j++)
+	{
+	  char x = copy[9-j][i];
+	matrix[i][j] = x;
+	}
+    }
+}
