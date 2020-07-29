82f05ce94768e829dcb04bbdf2c2c63f0a4f9641
Coursera Learner <nobody@nowhere.nul>
2020-07-29 14:10:06 +0000

    assignment poker 3

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -3,17 +3,48 @@
 #include <assert.h>
 #include "deck.h"
 void print_hand(deck_t * hand){
-
+  for(int i=0; i<hand->n_cards ; i++)
+    {
+      
+      print_card(*(hand->cards[i]));
+      printf(" ");
+    }
 }
 
 int deck_contains(deck_t * d, card_t c) {
+
+  for(int i=0; i<d->n_cards; i++)
+    {
+      if( d->cards[i]->value == c.value && d->cards[i]->suit == c.suit)
+	{
+	  return 1;
+	}
+    }
   return 0;
 }
 
 void shuffle(deck_t * d){
 
+  for(int i=0; i<d->n_cards; i++)  {
+    
+      int r = rand()% d->n_cards;
+      card_t *temp = d->cards[i];
+      d->cards[i] = d->cards[r];
+      d->cards[r] = temp;
+}
 }
 
 void assert_full_deck(deck_t * d) {
 
+  for(int i=2; i<=VALUE_ACE; i++) {
+
+    for(int j=SPADES; j<=CLUBS; j++) {
+
+      card_t temp;
+      temp.value = i;
+      temp.suit = j;
+      assert(deck_contains(d, temp));
+    }
+  }
+    
 }
