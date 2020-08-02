a2660a45b92229d62e7cbe2ba78de4bc08f0cc34
Coursera Learner <nobody@nowhere.nul>
2020-08-02 16:22:23 +0000

    assignment 25

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
   void Decrypt(char * arr , size_t n){
@@ -82,5 +82,65 @@ int main(int argc, char ** argv)
     perror("Failed to close the input file!");
     return EXIT_FAILURE;
   }
+  return EXIT_SUCCESS;
+}
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+size_t largestindx(int * array, size_t n){
+
+  size_t indx=0;
+
+  for (int i=1;i<n;i++){
+    if (array[indx] < array[i]){
+      indx = i;
+    }
+  }
+  return indx;
+}
+
+
+int main(int argc, char ** argv) {
+
+
+  if (argc != 2) {
+    fprintf(stderr,"Invalid argc\n");
+    return EXIT_FAILURE;
+  }
+
+  FILE * f = fopen(argv[1], "r");
+
+  if (f == NULL) {
+    fprintf(stderr,"Could not open file\n");
+    return EXIT_FAILURE;
+  }
+
+  int freq[26]={0};
+  int c;
+  //xint n = 0;
+
+  while (((c = fgetc(f)) != EOF)){
+
+    if (isalpha(c)){
+      freq[c-97] = freq[c-97] + 1;
+    }
+  }
+
+  size_t i = largestindx(freq, 26);
+
+  int key = (i+22)%26;
+
+
+  printf("%d\n",key);
+
+
+  if (fclose(f) != 0) {
+    fprintf(stderr,"Failed to close the input file!\n");
+    return EXIT_FAILURE;
+  }
+
   return EXIT_SUCCESS;
 }
