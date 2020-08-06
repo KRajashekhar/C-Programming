6985d9324c93cb5b706da998f738c78444a30d16
Coursera Learner <nobody@nowhere.nul>
2020-08-06 07:58:19 +0000

    assignment 30

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -19,6 +19,49 @@ void sortData(char ** data, size_t count) {
 int main(int argc, char ** argv) {
   
   //WRITE YOUR CODE HERE!
-  
+  char *line = NULL;
+  size_t sz ;
+  char **data = NULL;
+  size_t len = 0;
+  size_t i=0;
+  if(argc == 1) {
+    while((len = getline(&line, &sz, stdin)) >= 0) {
+
+      data = realloc(data,(i+1)*sizeof(*data));
+      data[i] = line;
+      line = NULL;
+      i++;
+    }
+    free(line);
+    sortData(data, i);
+  }
+  else{
+    for(int j = 1; j<argc; j++) {
+      FILE * f = fopen(argv[j], "r");
+      if(f == NULL){
+	fprintf(stderr,"could not open file\n");
+	return EXIT_FAILURE;
+      }
+      while((len == getline(&line, &sz,f)) >=0 ) {
+
+	data = realloc(data, (i+1)*sizeof(*data));
+	data[i] = line;
+	line = NULL;
+	i++;
+      }
+      free(line);
+      if(fclose(f) != 0) {
+	fprintf(stderr,"Failed to close the file\n");
+	return EXIT_FAILURE;
+      }
+    }
+    sortData(data,i);
+  }
+  for(int j=0; j<i; j++) {
+
+    printf("%s\n",data[i]);
+    free(data[i]);
+  }
+  free(data);
   return EXIT_SUCCESS;
 }
