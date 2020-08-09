05bbf8cbc26d0a4358216857b8d869d50843d23e
Coursera Learner <nobody@nowhere.nul>
2020-08-09 10:26:22 +0000

    c4prj2_input

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -6,7 +6,7 @@
 #include "deck.h"
 #include "future.h"
 
-void add_future_card(future_cards_t *fc, size_t index; card_t *ptr) {
+void add_future_card(future_cards_t *fc, size_t index, card_t *ptr) {
   if(index < fc->n_decks) {
     fc->decks[index].n_cards++;
     fc->decks[index].cards = realloc(fc->decks[index].cards,fc->decks[index].n_cards * sizeof(*fc->decks[index].cards));
